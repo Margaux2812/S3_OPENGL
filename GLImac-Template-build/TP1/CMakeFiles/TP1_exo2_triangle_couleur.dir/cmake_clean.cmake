c2aeadbb9d34bb1094625fafbf5243ec55e86096
Margaux <margauxvllt@gmail.com>
2019-09-20 18:45:48 +0200

    1ere seance

---
+++ GLImac-Template-build/TP1/CMakeFiles/TP1_exo2_triangle_couleur.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_exo2_triangle_couleur.dir/exo2_triangle_couleur.cpp.o"
+  "TP1_exo2_triangle_couleur.pdb"
+  "TP1_exo2_triangle_couleur"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1_exo2_triangle_couleur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
